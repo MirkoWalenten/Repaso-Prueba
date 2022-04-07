2fee3b4548b03b15b854471e8efbe57227f3f3a9
MirkoWalenten <101577586+MirkoWalenten@users.noreply.github.com>
2022-04-07 08:56:04 -0300

    Finished

---
+++ Assets/Scripts/Repaso1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Crea un programa que permita ingresar por inspector dos valores enteros y responda si son iguales o no lo son.
+public class Repaso1 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los numeros ingresados son iguales");
+        } 
+        else
+        {
+            Debug.Log("Los numeros ingresados NO son iguales");
+        }
+         
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Repaso2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repaso2 : MonoBehaviour
+{
+    public int prod1;
+    public int prod2;
+    public int prod3;
+    public int dinero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int suma;
+        int total;
+        suma = prod1 + prod2 + prod3;
+        
+
+        if(suma > dinero)
+        {
+            Debug.Log("Los productos superan el monto de dinero disponible");
+            total = suma - dinero;
+            Debug.Log("Lo que te falta es " + total);
+          
+        }
+        else
+        {
+            Debug.Log("Los productos NO superan el monto disponible");
+            int sobra;
+            sobra = (dinero - suma);
+            Debug.Log("Lo que le sobra es" + sobra);
+        }
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
